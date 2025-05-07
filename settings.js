2fef081fab75233fae2d70322c4de09de5f2f804
Fahrudin <muhammadfahrudin0908@gmail.com>
2025-05-07 21:09:30 +0700

    settings.js

---
+++ settings.js
@@ -0,0 +1,113 @@
+const fs = require("fs");
+const chalk = require("chalk");
+global.WHATSAPP_API = "@ryzenofc/baileys";
+
+global.botName = "ᴛᴏʏᴀ ʙᴏᴛ";
+global.ownerNumber = "6281390655891";
+global.ownerName = "sᴇɴᴢʏ ᴏғᴄ";
+global.website = "https://api.whatsapp.com/send?phone=+6281390655891";
+global.wagc = "https://chat.whatsapp.com/Ca1RcXhtbh67y6WEZdb42Y";
+
+global.packname = "ᴛᴏʏᴀ ʙᴏᴛ";
+global.author = "ᴅɪɴᴢ ᴏғᴄ";
+global.footer = "© 2025 · ᴅɪɴᴢ ᴏғᴄ";
+global.creator = "6281390655891@s.whatsapp.net";
+global.owner = ["6281390655891"];
+global.pbal.saluran = "120363372841212569@newsletter";
+global.saluranName = "ALL INFO ᴅɪɴᴢᴢ";remium = ["6281390655891"];
+global.prefa = ".";
+global.tempatDB = "database.json";
+
+global.sessionName = "session";
+
+global.panel = "https://panel.maelyn.app";
+global.cred = "ptla_NyT53XIGfvyK3ptQuOCocuzjEQUi79kep5rT91QVBYI";
+global.apiuser = "ptlc_2Ab5V8x9hlevFNXKpuPKsoQVN3EdtL2L2EPvpGVzsmS";
+global.eggs = "15";
+global.nets = "5";
+global.location = "1";
+
+global.CF_API_KEY = "kDVfZ8NWOFNCsloIA6ckd58duuYZ0nlXwysSn9nV";
+global.CF_ZONE_ID = "b9883610d0c1ecf9c83f002897822682";
+global.CF_DOMAIN = "maelyn.my.id";
+
+global.APP_EMAIL = "cs.moraai@gmail.com";
+global.APP_PASSWORD = "rwak vblz ttol ftdx";
+
+global.typemenu = "v14";
+global.typereply = "v6";
+global.autoblocknumber = "62";
+global.antiforeignnumber = "62";
+global.welcome = false;
+global.anticall = true;
+global.autoswview = true;
+global.adminevent = true;
+global.groupevent = true;
+global.notifRegister = true;
+global.onlyRegister = false;
+
+global.payment = {
+    dana: "087779101885",
+    gopay: "087779101885",
+    ovo: "-",
+    qris: "-"
+};
+
+global.limit = {
+    free: 20,
+    premium: "Infinity",
+    vip: "Infinity"
+};
+
+global.uang = {
+    free: 10000,
+    premium: 1000000000,
+    vip: 1000000000
+};
+
+global.bot = {
+    limit: 1,
+    uang: 1
+};
+
+global.game = {
+    suit: {},
+    menfes: {},
+    tictactoe: {},
+    kuismath: {},
+    tebakbom: {}
+};
+
+global.mess = {
+  limit: "ᴇᴇʜ~ ᴋᴀᴍᴜ ᴜᴅᴀʜ ᴍᴇɴᴄᴀᴘᴀɪ ʙᴀᴛᴀs ᴘᴇɴɢɢᴜɴᴀᴀɴ! sᴀʙᴀʀ ᴅᴜʟᴜ ʏᴀᴀ, ᴄᴏʙᴀ ʟᴀɢɪ ɴᴀɴᴛɪ~ 😣✨",
+  nsfw: "ʜᴍᴘʜ! ᴋᴏɴᴛᴇɴ ᴅᴇᴡᴀsᴀ ɴɢɢᴀᴋ ᴅɪɪᴢɪɴᴋᴀɴ ᴅɪ sɪɴɪ, ᴛᴀʜᴜ! ᴛᴀɴʏᴀᴋᴀɴ ᴀᴅᴍɪɴ ᴅᴜʟᴜ, ᴅᴀsᴀʀ ᴍᴇsᴜᴍ~ 😳🚫",
+  done: "ʜᴇʜᴇ~ ʙᴇʀʜᴀsɪʟ ᴅᴇʜ! ᴍᴜᴅᴀʜ ᴋᴀɴ? ᴊᴀɴɢᴀɴ ᴛᴇʀʟᴀʟᴜ ʙᴇʀɢᴀɴᴛᴜɴɢ ᴘᴀᴅᴀᴋᴜ ʏᴀ~ 😏💕",
+  error: "ᴜɢʜ... ᴀᴅᴀ ʏᴀɴɢ sᴀʟᴀʜ ɴɪʜ! ᴄᴏʙᴀ ʟᴀɢɪ ʏᴀ, ᴊᴀɴɢᴀɴ ɴʏᴇʀᴀʜ! 😖💦",
+  success: "ʏᴀᴛᴛᴀ~! ʙᴇʀʜᴀsɪʟ~ ɴɪʜ ʜᴀsɪʟɴʏᴀ ʙᴜᴀᴛᴍᴜ~ 😆🎉",
+  owner: "ᴇɪᴛs, ᴄᴜᴍᴀ ᴘᴇᴍɪʟɪᴋᴋᴜ ʏᴀɴɢ ʙᴏʟᴇʜ ᴘᴀᴋᴀɪ ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ~ ᴊᴀɴɢᴀɴ ɴᴀᴋᴀʟ ʏᴀ~ 😤💢",
+  botAdmin: "ᴀᴋᴜ ʙᴜᴛᴜʜ ᴊᴀᴅɪ ᴀᴅᴍɪɴ ᴅᴜʟᴜ ɴɪʜ ʙᴜᴀᴛ ɪᴛᴜ! ᴄᴇᴘᴇᴛᴀɴ ᴀᴛᴜʀ ᴅᴏɴɢ~ 😒👮‍♀️",
+  admin: "ᴄᴜᴍᴀ ᴀᴅᴍɪɴ ɢʀᴜᴘ ʏᴀɴɢ ʙᴏʟᴇʜ, ᴋᴀᴍᴜ ʙᴇʟᴜᴍ ᴄᴜᴋᴜᴘ ᴋᴇʀᴇɴ ʙᴜᴀᴛ ɪᴛᴜ~ 😜✨",
+  group: "ᴘᴇʀɪɴᴛᴀʜ ɪɴɪ ᴄᴜᴍᴀ ʙᴜᴀᴛ ᴅɪ ɢʀᴜᴘ ʏᴀ, ᴊᴀɴɢᴀɴ ɪsᴇɴɢ ᴅɪ ᴛᴇᴍᴘᴀᴛ ʟᴀɪɴ~ 🙅‍♀️",
+  private: "ʜᴀɴʏᴀ ʙɪsᴀ ᴅɪ ᴄʜᴀᴛ ᴘʀɪʙᴀᴅɪ, ᴊᴀᴅɪ ʙɪsɪᴋ-ʙɪsɪᴋ ᴀᴊᴀ ʏᴀ~ 🤫💕",
+  bot: "ᴄᴜᴍᴀ ʙᴜᴀᴛ ʙᴏᴛ ᴀᴊᴀ sɪʜ~ ᴋᴀᴍᴜ ɴɢɢᴀᴋ ʙɪsᴀ~ 😝🤖",
+  wait: "ᴜ-ᴜʜ... sᴀʙᴀʀ ᴅɪᴋɪᴛ ᴅᴏɴɢ! ᴀᴋᴜ ʟᴀɢɪ ᴘʀᴏsᴇs ɴɪʜ~ 😣⏳",
+  premium: "ғɪᴛᴜʀ ɪɴɪ ʙᴜᴀᴛ ᴘᴇɴɢɢᴜɴᴀ ᴘʀᴇᴍɪᴜᴍ ᴀᴊᴀ~ ʏᴜᴋ ᴜᴘɢʀᴀᴅᴇ ᴅᴜʟᴜ~ ✨👑",
+  banned: "ʜᴇʜ, ᴋᴀᴍᴜ ᴋᴇɴᴀ ʙᴀɴɴᴇᴅ! ᴘᴀsᴛɪ ɴᴀᴋᴀʟ ʙᴀɴɢᴇᴛ sɪʜ... 😤🚫",
+  unban: "ʏᴇᴀʏ! ᴋᴀᴍᴜ ᴜᴅᴀʜ ᴅɪ-ᴜɴʙᴀɴ~ ᴊᴀɴɢᴀɴ ɴᴀᴋᴀʟ ʟᴀɢɪ ʏᴀᴀ~ 😆✨",
+  restrict: "ғɪᴛᴜʀ ɪɴɪ ɴɢɢᴀᴋ ᴀᴋᴛɪғ ᴅɪ sɪɴɪ. ɴɢᴇʀᴛɪ ᴋᴀɴ? ᴊᴀɴɢᴀɴ ᴍᴀᴋsᴀ~ 😏❌"
+};
+
+global.imageDonasi = "";
+global.imageUrl = "https://img1.pixhost.to/images/5161/589913629_skyvien.jpg";
+global.imageBuffer = fs.readFileSync("./media/imageBuffer.jpg");
+global.videoBuffer = fs.readFileSync("./media/videoBuffer.mp4");
+global.audioBuffer = fs.readFileSync("./media/audioBuffer.mp3");
+
+let file = require.resolve(__filename);
+fs.watchFile(file, () => {
+    fs.unwatchFile(file);
+    console.log(chalk.yellow.bold(`\n⚠️ ${__filename} telah diperbarui! ⚠️`));
+    console.log(chalk.green("🔄 Silakan restart bot untuk menerapkan perubahan.\n"));
+    delete require.cache[file];
+    require(file);
+});
